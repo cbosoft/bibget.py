6ba11d687d395cb43400f1d6d3add5e2811620fe
Christopher <chrismjboyle@gmail.com>
2018-06-25 16:07:51 +0100

    Started to add pdf scraper

--- bibget.py
+++ bibget.py
@@ -1,10 +1,61 @@
+#!/usr/env/bin python3
+'''
+bibget.py
+
+Gets bibtex formatted citation data from pdf
+
+Usage:
+  bibget.py <doc>
+'''
+
+from docopt import docopt
 import requests
 from bs4 import BeautifulSoup
+import re
+import io
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+
+def convertPDFToText(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=0, password='', caching=True, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
 
 def getBibStr(data : dict):
+
+    datekeys = [
+        'date',
+        'publication_date'
+    ]
+    
+    
     # assumes article right now
     bibStr = "@article{"
-    year = data['date'][:4]
+    try:
+        year = data['date'][:4]
+    except KeyError:
+        year = data['publication_date'][:4]
     key = data['author'][0].split(' ')[-1] + year
     bibStr += key
     bibStr += "\n\t"
@@ -28,12 +79,46 @@ def getBibStr(data : dict):
     return bibStr
     
 
+def getDOI(pdfPath : str):
+    text = convertPDFToText(pdfPath)
+    lines = text.split('\n')
+    regex = re.compile(r"doi.org\/\d+\.\d+\/[\d\w]+")
+    possible_dois = list()
+    for line in lines:
+        match = regex.search(line)
+        if match != None: possible_dois.append(match.group())
+
+    if len(possible_dois) == 0:
+        raise Exception("No DOI found in PDF!")
+
+    if len(possible_dois) == 1:
+        return possible_dois[0]
+
+    doi_check = dict()
+    for doi in possible_dois:
+        if doi not in doi_check.keys():
+            doi_check[doi] = 1
+        else:
+            doi_check[doi] += 1
+
+    if len(doi_check.keys()) == 1:
+        return possible_dois[0]
+
+    maxv = 0
+    atk = ""
+    for k, v in doi_check:
+        if v > maxv:
+            maxv = v
+            atk = k
+    return k
+
     
 def getBibData(doi):
     r = requests.get("https://doi.org/"+doi)
     r.encoding = 'utf-8'
     soup = BeautifulSoup(str(r.text), 'html.parser')
     metas = soup.find_all('meta')
+    print(metas)
     bibdata = dict()
     for meta in metas:
         try:
@@ -53,14 +138,11 @@ def getBibData(doi):
 
         
 if __name__ == "__main__":
-    test_dois = [
-        "10.1103/PhysRevLett.112.098302",
-        "10.1103/PhysRevLett.75.2148"
-    ]
-    for doi in test_dois:
-        bibData = getBibData(doi)
+    args = docopt(__doc__, version="1")
+    pdfPath = args['<doc>']
+    doi = getDOI(pdfPath)
+    bibData = getBibData(doi)
 
-        #for k, v in bibData.items():
-        #    print(k, v)
-
-        print(getBibStr(bibData))
+    for k, v in bibData.items():
+        print(k, v)
+    #print(getBibStr(bibData))
